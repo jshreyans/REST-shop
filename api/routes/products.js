af211c777af79d966aeea094406e4666d407b135
Shreyans Jain <shreyansj551@gmail.com>
2019-04-02 19:43:59 +0530

    Adding mongodb to routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -27,27 +27,31 @@ router.post('/', (req,res,next) => {
     }).catch((err) => {
         console.log(err);
     });
-    
+
     res.status(201).json({
         message: 'This is a POST request to /products',
         createdProduct: product
     });
 });
 
-router.get('/:productID', (req,res,next) => {
-    const id = req.params.productID;
-
-    if(id === 'special') {
-        res.status(200).json({
-            message: 'You have reached a special place',
-            id: id
-        });
-    }
-    else {
-        res.status(200).json({
-            message: 'You passed a simlpe id to the products',
-        });
-    }
-});
+router.get("/:productId", (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+      .exec()
+      .then(doc => {
+        console.log("From database", doc);
+        if (doc) {
+          res.status(200).json(doc);
+        } else {
+          res
+            .status(404)
+            .json({ message: "No valid entry found for provided ID" });
+        }
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: err });
+      });
+  });
 
 module.exports = router;
