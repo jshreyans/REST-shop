d56ddedc50b0604f771e2795912bdf751671a422
Shreyans Jain <shreyansj551@gmail.com>
2019-04-12 11:09:24 +0530

    correct import path for user model

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
-const User = require('..//models/user');
+const User = require('../models/user');
 
 exports.user_signup = (req,res,next) => {
     User.find({email: req.body.email})
