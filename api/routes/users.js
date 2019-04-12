bfb326fb0d26611b18b592065a1b2ea4a53100c8
Shreyans Jain <shreyansj551@gmail.com>
2019-04-12 11:16:23 +0530

    add auth middleware to user deletion route

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const UsersController = require('../controllers/users');
+const checkAuth = require('../middleware/check-auth');
 
 router.post('/signup', UsersController.user_signup);
 
 router.post('/login', UsersController.user_login);
 
-router.delete('/:userID', UsersController.delete_user);
+router.delete('/:userID', checkAuth, UsersController.delete_user);
 
 module.exports = router;
